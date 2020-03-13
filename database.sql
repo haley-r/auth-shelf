28e6ca7d6ba67413ecef3f51955946396ec9ce05
Derek Boat <derek.boat@gmail.com>
2020-03-13 11:09:33 -0500

    oooof

--- database.sql
+++ database.sql
@@ -15,3 +15,8 @@ CREATE TABLE "item" (
     "image_url" VARCHAR (2083),
     "user_id" INT REFERENCES "user"
 );
+
+INSERT INTO "item" ("description", "image_url")
+VALUES ('flour', 'https://target.scene7.com/is/image/Target/GUEST_12820de3-24e3-4822-abf4-d823084d5c26?wid=488&hei=488&fmt=pjpeg'),
+('sugar', 'https://images-na.ssl-images-amazon.com/images/I/71oM62bfyRL._SL1000_.jpg'),
+('butter', 'https://storcpdkenticomedia.blob.core.windows.net/media/lolretail/media/lolr-media/article_images/2017_december_salted-vs-unsalted-sticks_jpg_2.jpg?ext=.jpg');
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Shelf Page
-    </p>
-  </div>
-);
+class InfoPage extends Component {
 
-export default InfoPage;
+  render() {
+    
+    return (
+      <div>
+        <p>
+          Shelf Page
+    {JSON.stringify(this.props.item)}
+          <ul>
+            
+            {this.props.item[0] ?
+            <p>hello</p>
+            :
+              <>
+                {this.props.item.map(shelfItem =>
+                  <li key= {shelfItem.id}>
+                    <img src={shelfItem.image_url} />
+                  </li>)} 
+                </>  
+             }
+          </ul>
+        </p>
+      </div>
+    )
+  }
+}
+const mapStateToProps = reduxState => ({
+  item: reduxState.item,
+});
+
+export default connect(mapStateToProps)(InfoPage);
